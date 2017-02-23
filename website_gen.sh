ed0619379cc551ed806d51d7c9e66c47bfff8c68
Christian krueger <cakrueger2@wisc.edu>
2017-02-23 02:28:26 +0000

    christian contribution

---
+++ website_gen.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# sub folders in /var/www/http
+# varible number of websites
+# vairble number of objects
+# varible size of objects
+# diffrent sizes in diffrent order
+# reorder files
+
+
