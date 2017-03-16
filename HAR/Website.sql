eb7222e8eb6c317a8e909bbf0f295eb440c28716
sjfricke <sjfricke@wisc.edu>
2017-03-15 22:50:59 -0500

    Added SQL table creates
    Used if needed to create the SQL tables

---
+++ HAR/Website.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `har_db`.`Website` (
+  `WebsiteInstance` INT NOT NULL AUTO_INCREMENT,
+  `Domain` VARCHAR(100) NOT NULL,
+  `NumberOfFiles` INT NOT NULL,
+  `StartedDateTime` DATETIME NOT NULL,
+  `OnContentLoad` FLOAT NOT NULL,
+  `OnLoad` FLOAT NOT NULL,
+  `ObjectType` VARCHAR(8) NOT NULL,
+  `Size` VARCHAR(8) NOT NULL,
+  `Count` VARCHAR(8) NOT NULL,
+  `Structure` VARCHAR(8) NOT NULL,
+  PRIMARY KEY (`WebsiteInstance`));
+
