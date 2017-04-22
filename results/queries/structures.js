16db4e7b4587127b6a0a41d6fda5635907272859
Ubuntu <ubuntu@ip-172-31-9-49.us-west-2.compute.internal>
2017-04-22 07:08:01 +0000

    fixed structure query

--- results/queries/structures.js
+++ results/queries/structures.js
@@ -2,47 +2,46 @@
 const COUNT = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "15", "20", "25", "30", "35", "50", "70", "90", "100", "125", "150", "175", "200"];
 // Each column has a index after saved for tooltips
 const COLUMNS = [
-	{ name : "1 MB - Nginx",	Size : 4, ConnectionPath : 1, Structure : "a", data : [] }, // 1
-	{ name : "1 MB - Apache",   	Size : 4, ConnectionPath : 2, Structure : "a", data : [] }, // 3
-        { name : "1 MB - Apache W/oSP",	Size : 4, ConnectionPath : 3, Structure : "a", data : [] }, // 5
-	{ name : "2 MB - Nginx",	Size : 6, ConnectionPath : 1, Structure : "a", data : [] }, // 7
-	{ name : "2 MB - Apache",   	Size : 6, ConnectionPath : 2, Structure : "a", data : [] }, // 9
-        { name : "2 MB - Apache W/oSP",	Size : 6, ConnectionPath : 3, Structure : "a", data : [] }, // 11
-	{ name : "4 MB - Nginx",  	Size : 8, ConnectionPath : 1, Structure : "a", data : [] }, // 13
-	{ name : "4 MB - Apache", 	Size : 8, ConnectionPath : 2, Structure : "a", data : [] }, // 15
-        { name : "4 MB - Apache W/oSP",	Size : 8, ConnectionPath : 3, Structure : "a", data : [] }, // 17 //
-     	{ name : "1 MB - Nginx",	Size : 4, ConnectionPath : 1, Structure : "b", data : [] }, // 19
-	{ name : "1 MB - Apache",   	Size : 4, ConnectionPath : 2, Structure : "b", data : [] }, // 21
-        { name : "1 MB - Apache W/oSP",	Size : 4, ConnectionPath : 3, Structure : "b", data : [] }, // 23
-	{ name : "2 MB - Nginx",	Size : 6, ConnectionPath : 1, Structure : "b", data : [] }, // 25
-	{ name : "2 MB - Apache",   	Size : 6, ConnectionPath : 2, Structure : "b", data : [] }, // 27
-        { name : "2 MB - Apache W/oSP",	Size : 6, ConnectionPath : 3, Structure : "b", data : [] }, // 29
-	{ name : "4 MB - Nginx",  	Size : 8, ConnectionPath : 1, Structure : "b", data : [] }, // 31
-	{ name : "4 MB - Apache", 	Size : 8, ConnectionPath : 2, Structure : "b", data : [] }, // 33
-        { name : "4 MB - Apache W/oSP",	Size : 8, ConnectionPath : 3, Structure : "b", data : [] }, // 35 //
-	{ name : "1 MB - Nginx",	Size : 4, ConnectionPath : 1, Structure : "c", data : [] }, // 37
-	{ name : "1 MB - Apache",   	Size : 4, ConnectionPath : 2, Structure : "c", data : [] }, // 39
-        { name : "1 MB - Apache W/oSP",	Size : 4, ConnectionPath : 3, Structure : "c", data : [] }, // 41
-	{ name : "2 MB - Nginx",	Size : 6, ConnectionPath : 1, Structure : "c", data : [] }, // 43
-	{ name : "2 MB - Apache",   	Size : 6, ConnectionPath : 2, Structure : "c", data : [] }, // 45
-        { name : "2 MB - Apache W/oSP",	Size : 6, ConnectionPath : 3, Structure : "c", data : [] }, // 47
-	{ name : "4 MB - Nginx",  	Size : 8, ConnectionPath : 1, Structure : "c", data : [] }, // 49
-        { name : "4 MB - Apache", 	Size : 8, ConnectionPath : 2, Structure : "c", data : [] }, // 51
-        { name : "4 MB - Apache W/oSP",	Size : 8, ConnectionPath : 3, Structure : "c", data : [] }, // 53 //
-	{ name : "1 MB - Nginx",	Size : 4, ConnectionPath : 1, Structure : "d", data : [] }, // 55
-	{ name : "1 MB - Apache",   	Size : 4, ConnectionPath : 2, Structure : "d", data : [] }, // 57
-        { name : "1 MB - Apache W/oSP",	Size : 4, ConnectionPath : 3, Structure : "d", data : [] }, // 59
-	{ name : "2 MB - Nginx",	Size : 6, ConnectionPath : 1, Structure : "d", data : [] }, // 61
-	{ name : "2 MB - Apache",   	Size : 6, ConnectionPath : 2, Structure : "d", data : [] }, // 63
-        { name : "2 MB - Apache W/oSP",	Size : 6, ConnectionPath : 3, Structure : "d", data : [] }, // 65
-	{ name : "4 MB - Nginx",  	Size : 8, ConnectionPath : 1, Structure : "d", data : [] }, // 67
-	{ name : "4 MB - Apache", 	Size : 8, ConnectionPath : 2, Structure : "d", data : [] }, // 69
-        { name : "4 MB - Apache W/oSP",	Size : 8, ConnectionPath : 3, Structure : "d", data : [] }  // 71
+	{ name : "Same Size",	Size : 4, ConnectionPath : 1, Structure : "a", data : [] }, // 1
+	{ name : "Same Size",  	Size : 4, ConnectionPath : 2, Structure : "a", data : [] }, // 3
+        { name : "Same Size",	Size : 4, ConnectionPath : 3, Structure : "a", data : [] }, // 5
+	{ name : "Same Size",	Size : 6, ConnectionPath : 1, Structure : "a", data : [] }, // 7
+	{ name : "Same Size",  	Size : 6, ConnectionPath : 2, Structure : "a", data : [] }, // 9
+        { name : "Same Size",	Size : 6, ConnectionPath : 3, Structure : "a", data : [] }, // 11
+	{ name : "Same Size",  	Size : 8, ConnectionPath : 1, Structure : "a", data : [] }, // 13
+	{ name : "Same Size", 	Size : 8, ConnectionPath : 2, Structure : "a", data : [] }, // 15
+        { name : "Same Size",	Size : 8, ConnectionPath : 3, Structure : "a", data : [] }, // 17 //
+     	{ name : "Ascending",	Size : 4, ConnectionPath : 1, Structure : "b", data : [] }, // 19
+	{ name : "Ascending",  	Size : 4, ConnectionPath : 2, Structure : "b", data : [] }, // 21
+        { name : "Ascending",	Size : 4, ConnectionPath : 3, Structure : "b", data : [] }, // 23
+	{ name : "Ascending",	Size : 6, ConnectionPath : 1, Structure : "b", data : [] }, // 25
+	{ name : "Ascending",  	Size : 6, ConnectionPath : 2, Structure : "b", data : [] }, // 27
+        { name : "Ascending",	Size : 6, ConnectionPath : 3, Structure : "b", data : [] }, // 29
+	{ name : "Ascending",  	Size : 8, ConnectionPath : 1, Structure : "b", data : [] }, // 31
+	{ name : "Ascending", 	Size : 8, ConnectionPath : 2, Structure : "b", data : [] }, // 33
+        { name : "Ascending",	Size : 8, ConnectionPath : 3, Structure : "b", data : [] }, // 35 //
+	{ name : "Descending",  Size : 4, ConnectionPath : 1, Structure : "c", data : [] }, // 37
+	{ name : "Descending", 	Size : 4, ConnectionPath : 2, Structure : "c", data : [] }, // 39
+        { name : "Descending",	Size : 4, ConnectionPath : 3, Structure : "c", data : [] }, // 41
+	{ name : "Descending",	Size : 6, ConnectionPath : 1, Structure : "c", data : [] }, // 43
+	{ name : "Descending", 	Size : 6, ConnectionPath : 2, Structure : "c", data : [] }, // 45
+        { name : "Descending",	Size : 6, ConnectionPath : 3, Structure : "c", data : [] }, // 47
+	{ name : "Descending", 	Size : 8, ConnectionPath : 1, Structure : "c", data : [] }, // 49
+        { name : "Descending", 	Size : 8, ConnectionPath : 2, Structure : "c", data : [] }, // 51
+        { name : "Descending",	Size : 8, ConnectionPath : 3, Structure : "c", data : [] }, // 53 //
+	{ name : "Random",	Size : 4, ConnectionPath : 1, Structure : "d", data : [] }, // 55
+	{ name : "Random",   	Size : 4, ConnectionPath : 2, Structure : "d", data : [] }, // 57
+        { name : "Random",	Size : 4, ConnectionPath : 3, Structure : "d", data : [] }, // 59
+	{ name : "Random",	Size : 6, ConnectionPath : 1, Structure : "d", data : [] }, // 61
+	{ name : "Random",   	Size : 6, ConnectionPath : 2, Structure : "d", data : [] }, // 63
+        { name : "Random",	Size : 6, ConnectionPath : 3, Structure : "d", data : [] }, // 65
+	{ name : "Random",  	Size : 8, ConnectionPath : 1, Structure : "d", data : [] }, // 67
+	{ name : "Random", 	Size : 8, ConnectionPath : 2, Structure : "d", data : [] }, // 69
+        { name : "Random",	Size : 8, ConnectionPath : 3, Structure : "d", data : [] }  // 71
 
 ];
 // all groups need zero index for the X column
 const GROUPS = [
-    
     { name : "1 MB - Nginx",        index : [0,1,2,19,20,37,38,55,56] },
     { name : "1 MB - Apache",       index : [0,3,4,21,22,39,40,57,58] },
     { name : "1 MB - Apache W/oSP", index : [0,5,6,23,24,41,42,59,60] },
@@ -60,19 +59,19 @@ var fs = require("fs");
 // Top part of HTML
 var HTML = "<html><head><title>HTTP2 Opimization</title></head>\n"+
     "<body>\n"+
-    "<div id='control_div'>";			
+    "<div id='control_div'>";
 
 // Adds buttons
 for (var i = 0; i < GROUPS.length; i++) {
     HTML += "<button style='marginRight:10px' "+
-	"onclick='changeView(["+ GROUPS[i].index +"])'"+
+	"onclick=\"changeView(["+ GROUPS[i].index +"], '" + GROUPS[i].name + "')\""+
 	">" + GROUPS[i].name + "</button> ";
 }
 
 HTML += "</div>\n"+
     "<div id='chart_div'></div>\n"+
     "</body>\n"+
-    "<script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>\n" + 
+    "<script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>\n" +
     "<script> var view, options, chart;\n" +
     "function drawChart(){\n" +
     "data = new google.visualization.DataTable();\n" +
@@ -88,27 +87,26 @@ module.exports = (connection, verbose) => {
     return new Promise(function(resolve, reject) {
     	// NEED to run
 		connection.query(
-	         `SELECT Website.WebsiteID, Size, Count, Structure, 
+	         `SELECT Website.WebsiteID, Size, Count, Structure,
              MAX(TotalTime), AVG(Send), AVG(Wait), AVG(Receive), ConnectionPath
              FROM Website INNER JOIN Entries ON Website.WebsiteID = Entries.WebsiteID  
              WHERE ResponseHttpVersion = "h2" AND (RequestUrl <> Entries.Domain) AND (Size = 4 OR Size = 6 OR Size = 8)
              GROUP BY WebsiteID, ConnectionPath
 	     ORDER BY Size, Structure, ConnectionPath;`,
 	    (error, results, fields) => {
-		
+
 		if (error) {
 		    console.error(error);
 		    return reject(false);
 		}
-		
+
 		if (verbose) { console.log("Query Successful"); }
-		
-		
-		
+
+
 		for (let i = 0; i < COLUMNS.length; i++) {
-		    
+
 		    for (let j = 0; j < COUNT.length; j++) {
-			
+
 			COLUMNS[i].data.push({
 			    "num_of_entries" : 0,
 			    "TotalTime" : 0,
@@ -118,9 +116,9 @@ module.exports = (connection, verbose) => {
 			});
 		    }
 		}
-		
+
 		if (verbose) { console.log("Column Data array initalized"); }
-		
+
 
 		// take queried data and add it to data
 		for (let i = 0; i < results.length; i++) {
@@ -197,7 +195,7 @@ module.exports = (connection, verbose) => {
 
 		// TODO give more colors choices
 		HTML += "options = {\n"+
-		    "title:'Load time over different number of files of same size',\n"+
+		    "title:'Load time over different structures',\n"+
 		    "hAxis:{title:'Files in Page'},\n"+
 		    "vAxis:{title:'Time (milliseconds)'},\n"+
 		    "colors:['Red','Green','Navy','Purple','DarkOrange','Black','Cyan','SteelBlue','Gold','SpringGreen'],\n"+
@@ -206,14 +204,15 @@ module.exports = (connection, verbose) => {
 		    "pointSize: 5\n"+
 		    "};\n"+ // end of option object
 		    "view = new google.visualization.DataView(data);\n"+
-		    "chart = new google.visualization.AreaChart(document.getElementById('chart_div'));\n"+
+		    "chart = new google.visualization.LineChart(document.getElementById('chart_div'));\n"+
 		    "chart.draw(view, options);\n"+
 		    "}\n"+ // end of drawChart()
-		    "google.charts.load('current', {packages: ['corechart']});\n"+
+		    "google.charts.load('current', {packages: ['corechart', 'line']});\n"+
 		    "google.charts.setOnLoadCallback(drawChart);\n"+
 		    // add change view function
-		    "function changeView(index) {\n"+
+		    "function changeView(index, name) {\n"+
 		    "view.setColumns( index );\n"+
+		    "options.title = 'Load Time Over Different Structures with ' + name + ' sites';\n"+
 		    "chart.draw(view, options);\n"+
 		    "}\n"+
 		    "</script>"+		    
